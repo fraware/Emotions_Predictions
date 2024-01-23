1250947a961b7e183466dd06bd7e6b8df28bfa3f
Matéo H. Petel <113530345+fraware@users.noreply.github.com>
2024-01-22 23:00:31 -0800

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,5 +1,5 @@
 # Define constants and configurations
-FILE_PATH = r"C:\Users\mateo\OneDrive\Documents\GAP YEAR 2022-2023\CNN EEG Project\emotions.csv"
+FILE_PATH = r"EEG-emotions.csv"
 LEARNING_RATE = 0.001
 BATCH_SIZE = 32
 TRAIN_SIZE = 0.7
